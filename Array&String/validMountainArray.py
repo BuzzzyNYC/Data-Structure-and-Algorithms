c187c6588aa38b600de04afd783d92fe00292bca
BuzzzyNYC <nguyentranglilo9192@gmail.com>
2022-01-07 10:32:06 -0500

    Created file

---
+++ Array&String/validMountainArray.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/valid-mountain-array/
+'''
+:type: List[arr]
+:rtype: Bool
+
+If we walk along the mountain from left to right, 
+we have to move strictly up, then strictly down.
+Algorithm: walk up from left to right until we can't: that has to be the peak. 
+We should ensure the peak is not the first or last element. 
+Then, we walk down. If we reach the end, the array is valid, otherwise its not.
+'''
+
+class Solution:
+    def validMountainArray(self, arr):
+        i = 0
+        # go up
+        while i < len(arr) and arr[i + 1] > arr[i]:
+            i += 1
+        # corner cases where the arr either goes up or down
+        if i == len(arr) - 1 or i == 0:
+            return False
+
+        # go down
+        while i < len(arr) and arr[i + 1] < arr[i]:
+            i + 1
+        if i == len(arr) - 1:
+            return True
+        else:
+            return False
